0f4b14d087e76ee509d5c6d8dabe845a01bd385c
RafaeNoor <abdurrafae98@live.com>
2023-08-22 16:55:27 -0500

    Updating Distributive Property generator and creating ExprParamUtils to extract parameter names

--- lib/properties/Distributive.py
+++ lib/properties/Distributive.py
@@ -4,14 +4,15 @@ import copy
 from  common.Types import *
 
 class Distributive(Property):
-  
-    
+
+
 
     def __init__(self, dsl_list = [], synth_desc = None):
         super().__init__(name = "Distributive", dsl_list = dsl_list, synth_desc = synth_desc)
-        
+
         # Memo table to avoid recomputing search for expressions producing output of size
         self.size_memo = {}
+        self.context_map = {}
 
 
 
@@ -20,7 +21,7 @@ class Distributive(Property):
         return "Check Distributive for DSLInstructions expression trees of depth 2"
 
     def get_dsl_insts_with_output_size(self, output_size):
-        """Return DSL Instructions which have produce the same output size as specified, and have 
+        """Return DSL Instructions which have produce the same output size as specified, and have
         at least two symbolic operands which are of the same size as output_size
 
         Args:
@@ -48,7 +49,7 @@ class Distributive(Property):
                 if num_matching_inputs >= 2:
                     matches.append(dsl_inst)
 
-        
+
         self.size_memo[str(output_size)] = matches
 
 
@@ -59,9 +60,9 @@ class Distributive(Property):
     def generate_candidates(self):
         """Generate list of 3-tuples where the first element is a DSLInstruction, the second element is also a DSLInstruction,
         and the third element is a pair of indices. The candidate to test is to check if the first DSLInstruciton can be distributed
-        across the second DSL Instruction, using the first element indices of the pairs. 
+        across the second DSL Instruction, using the first element indices of the pairs.
 
-        For example: (+ , *, [0,1]) 
+        For example: (+ , *, [0,1])
         can be interpreted as checking if:
         A + (C * D) =? (A * C) + (A * D) // Operand at Index 0 is a symbolic hole A and Operand at Index 1 is second DSLInstruction
 
@@ -84,9 +85,11 @@ class Distributive(Property):
                 inner_matches = self.get_dsl_insts_with_output_size(bv_size)
 
                 for match in inner_matches:
+                    if "mask" in dsl_inst.name or "mask" in match.name:
+                        continue
                     tuples.append((dsl_inst, match, pairs))
 
-                
+
 
         return tuples
 
@@ -99,9 +102,9 @@ class Distributive(Property):
         """
         context_pairs = [set(self.get_distributable_args_candidates_context(ctx)) for ctx in dsl_inst.contexts]
 
-        
+
         intersected_pairs = context_pairs[0]
-        
+
         for pairs in context_pairs[1:]:
             intersected_pairs = intersected_pairs.intersection(pairs)
 
@@ -122,7 +125,7 @@ class Distributive(Property):
             ctx (Context): Specific Context from a DSLInstruction.
         """
 
-        # Map from input size to 
+        # Map from input size to
         arg_sizes_map = {}
         for idx, arg in enumerate(ctx.context_args):
             if isinstance(arg, ConstBitVector) or isinstance(arg, BitVector):
@@ -153,16 +156,16 @@ class Distributive(Property):
         outer_dsl_inst = candidate[0]
         inner_dsl_inst = candidate[1]
         pair = candidate[2]
-        
+
         outer_sample_ctx = outer_dsl_inst.get_sample_context()
         inner_sample_ctx = inner_dsl_inst.get_sample_context()
 
         print("Checking if Distributivity holds for", outer_dsl_inst.name, "over", inner_dsl_inst.name ,"on", pair)
 
-  
+
         # We know pairs are of the same size so we have to get the bitvector size
         bv_size = inner_sample_ctx.context_args[pair[0]].size
-            
+
         reg_0 = Reg("0" , 16, bv_size)
         reg_1 = Reg("1" , 16, bv_size)
         reg_2 = Reg("2" , 16, bv_size)
@@ -170,7 +173,7 @@ class Distributive(Property):
         regs = [reg_0, reg_1, reg_2]
 
         vector_args = [bv_size, bv_size, bv_size]
-        
+
         # Create expression of the first form. For example: A * (B + C) for (*, +, [0,1])
 
         # e.g. B + C
@@ -193,14 +196,14 @@ class Distributive(Property):
         form_1_outer_expression.context_args[pair[0]] = reg_0
         form_1_outer_expression.context_args[pair[1]] = form_1_inner_expression
 
-        
+
         # Create expression of the second form. For example: (A * B) + (A *C) for (*, +, [0,1])
 
         # Left expression e.g. (A * B)
         form_2_inner_left_expression =  copy.deepcopy(outer_sample_ctx)
         form_2_inner_left_expression.context_args[pair[0]] = reg_0
         form_2_inner_left_expression.context_args[pair[1]] = reg_1
-        
+
         # Right expression e.g. (A * C)
         form_2_inner_right_expression =  copy.deepcopy(outer_sample_ctx)
         form_2_inner_right_expression.context_args[pair[0]] = reg_0
@@ -228,7 +231,7 @@ class Distributive(Property):
         # ensure that such symbolic bitvectors refer to the same operand.
 
         outer_other_indices = []
-        
+
         for idx, arg in enumerate(form_1_outer_expression.context_args):
             if isinstance(arg, BitVector) :
                 outer_other_indices.append(idx)
@@ -262,27 +265,27 @@ class Distributive(Property):
 
     # TODO: Masked operations are distributive only if their mask variables are made the same
 
-  
 
-                
+
+
 
         property_holds = check_if_contexts_equal(form_1_outer_expression, form_2_outer_expression, outer_dsl_inst, inner_dsl_inst, vector_args, self.synth_desc)
+
+        if property_holds.returncode == 0:
+            self.context_map[self.serialize_candidate(candidate)] = (form_1_outer_expression, form_2_outer_expression)
 
         print(property_holds.returncode == 0)
         return property_holds.returncode == 0
 
 
     def serialize_candidate(self, candidate):
-        return (candidate[0].name + "+" + candidate[1].name)
+        return (candidate[0].name + "+" + candidate[1].name+"+"+str(candidate[2]))
 
     def get_property_on_candidate(self, candidate):
-        return {"candidate": candidate[0].name + candidate[1].name, "indices": candidate[2]}
-
-
-            
-
-
-        
+        key = self.serialize_candidate(candidate)
+        input_expression, output_expression = self.context_map[key]
+        return {"candidate": candidate[0].name + candidate[1].name, "indices": candidate[2], "input_expression": input_expression.emit_context_expr_string(),
+                "output_expression": output_expression.emit_context_expr_string()}
 
 
 
@@ -290,4 +293,10 @@ class Distributive(Property):
 
 
 
-    
+
+
+
+
+
+
+
---
+++ lib/utils/ExprParamUtils.py
@@ -0,0 +1,135 @@
+import common.Types
+from  common.Instructions import Context
+import subprocess
+import os
+import tempfile
+import glob
+
+
+def class_name(arg):
+    if isinstance(arg, ConstBitVector):
+        return "const_bv"
+    elif isinstance(arg,LaneSize):
+        return "lane_size"
+    elif isinstance(arg, Precision):
+        return "prec"
+    elif isinstance(arg, Integer):
+        return "int"
+    elif isinstance(arg, Bool):
+        return "bool"
+    elif isinstance(arg, Reg):
+        return "reg"+str(arg.index)
+    else:
+        print("Unable to identify class name for:\t", arg)
+        assert False, "Unsupported class name type"
+
+def serialize_operand_for_param_map(expr, arg):
+    for idx, ctx_arg in enumerate(expr.context_args):
+        if ctx_arg != arg:
+            continue
+
+        if isinstance(arg, Reg):
+            return class_name(arg)
+        else:
+            return expr.name+"_"+class_name(arg)+"_"+str(idx)
+
+
+
+def find_integer_arg_with_same_value(input_map, reverse_map,  value):
+    for key, operand in reverse_map.items():
+        if isinstance(operand, Integer):
+
+            if operand.value == value:
+                return key
+    return None
+
+
+def find_lane_size_arg_with_same_value(input_map, reverse_map,  value):
+    for key, operand in reverse_map.items():
+        if isinstance(operand, LaneSize):
+            if operand.value == value:
+                return key
+    return None
+
+
+def find_precision_arg_with_same_value(input_map, reverse_map,  value):
+    for key, operand in reverse_map.items():
+        if isinstance(operand, Precision):
+            if operand.value == value:
+                return key
+    return None
+
+
+def find_arg_with_same_value(input_map, reverse_map, input_arg):
+
+    if isinstance(arg, Integer):
+        return find_integer_arg_with_same_value(input_map, reverse_map, input_arg.value)
+    elif isinstance(arg, Precision):
+        return find_precision_arg_with_same_value(input_map, reverse_map, input_arg.value)
+
+    elif isinstance(arg, LaneSize):
+        return find_lane_size_arg_with_same_value(input_map, reverse_map, input_arg.value)
+    else:
+        assert False, "Unsupported argument type: " + str(input_arg)
+
+
+
+
+# Creates a map from leaf parameters of expressions
+# to free variable names.
+def generate_parameter_map(expr1, expr2, input_map = {}, reverse_map = {}):
+    current_map = input_map
+    current_reverse_map = reverse_map
+
+
+
+    for idx, e1 in enumerate(expr1.context_args):
+
+        if isinstance(e1, Context):
+            updated_map , updated_reverse_map = generate_parameter_map(e1, expr2, input_map = current_map, reverse_map = current_reverse_map)
+            current_map, current_reverse_map  = updated_map, updated_reverse_map
+        elif isinstance(e1, Reg) or isinstance(e1, ConstBitVector):
+            serialized_name = serialize_operand_for_param_map(expr1, e1)
+            current_map[serialized_name] = serialized_name
+            current_reverse_map[serialized_name] = e1
+        else:
+
+
+            serialized_name = serialize_operand_for_param_map(expr1, e1)
+            matching_arg_name = find_arg_with_same_value(current_input_map, current_reverse_map, e1.value)
+            if matching_arg_name != None:
+                current_map[serialized_name] = current_map[matching_arg_name]
+            else:
+                current_map[serialized_name] = serialized_name
+                current_reverse_map[serialized_name] = e1
+
+
+
+    # Repeat for expression expr2. TODO: Refactor into single loop
+
+    for idx, e2 in enumerate(expr2.context_args):
+
+        if isinstance(e2, Context):
+            updated_map , updated_reverse_map = generate_parameter_map(expr1, e2, input_map = current_map, reverse_map = current_reverse_map)
+            current_map, current_reverse_map  = updated_map, updated_reverse_map
+        elif isinstance(e2, Reg) or isinstance(e2, ConstBitVector):
+            serialized_name = serialize_operand_for_param_map(expr1, e2)
+            current_map[serialized_name] = serialized_name
+            current_reverse_map[serialized_name] = e2
+        else:
+
+
+            serialized_name = serialize_operand_for_param_map(expr2, e2)
+            matching_arg_name = find_arg_with_same_value(current_input_map, current_reverse_map, e2.value)
+            if matching_arg_name != None:
+                current_map[serialized_name] = current_map[matching_arg_name]
+            else:
+                current_map[serialized_name] = serialized_name
+                current_reverse_map[serialized_name] = e2
+
+
+    return current_map, current_reverse_map
+
+
+
+
