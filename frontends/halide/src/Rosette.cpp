f677939b6dfabb7e4252ecc949f4c4ddff592b4b
RafaeNoor <abdurrafae98@live.com>
2024-11-11 13:02:25 -0600

    Removing empty space when emitting vector reduce from halide

--- frontends/halide/src/Rosette.cpp
+++ frontends/halide/src/Rosette.cpp
@@ -1115,7 +1115,7 @@ public:
         size_t iprec = VecTy.bits();
         size_t isize = VecTy.lanes() * VecTy.bits();
         std::string type_info = std::to_string(iprec) + " " + std::to_string(isize);
-        return tabs() + "(typed:signed-vector_reduce_ " + rkt_op + " " +
+        return tabs() + "(typed:signed-vector_reduce_" + rkt_op + " " +
                std::to_string(op->value.type().lanes() / op->type.lanes()) + "\n" + rkt_val+ " " +type_info + ")";
     }
 };
