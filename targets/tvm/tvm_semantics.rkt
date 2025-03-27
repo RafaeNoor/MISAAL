4ff019b907fbac75e2cf1d4a730a4cc14e3b7482
Alan Zhang <alanyz2@illinois.edu>
2025-03-27 16:33:44 -0500

    Semantics for all currently available ops

--- targets/tvm/tvm_semantics.rkt
+++ targets/tvm/tvm_semantics.rkt
@@ -6,6 +6,9 @@
 (require rosette/lib/destruct)
 (require hydride)
 
+; Missing [vec-mod, vec-gt, vec-ge, vec-eq, vec-bwor] semantics in this file but shouldn't matter
+
+
 (define (typed:vec-add v1 v2 iprec isize sign)
   (define dst
     (apply 
@@ -62,6 +65,120 @@
                      %narrow
                      )
            )
+    )
+  dst
+  )
+
+(define (typed:vec-div v1 v2 iprec isize sign)
+  (define dst
+    (apply 
+      concat
+           (for/list ([%iter (reverse (range 0 isize iprec))])
+                     (define %lastidx1 (- iprec 1))
+                     (define %high (+ %lastidx1 %iter))
+                     (define slice_v1 (extract %high %iter v1))
+                     (define %lastidx2 (- iprec 1))
+                     (define %high2 (+ %lastidx2 %iter))
+                     (define slice_v2 (extract %high2 %iter v2))
+                     (define %result (bvdiv slice_v1 slice_v2 sign))
+                     %result
+                     )
+           )
+    )
+  dst
+  )
+
+(define (typed:vec-min v1 v2 iprec isize sign)
+  (define dst
+    (apply 
+      concat
+           (for/list ([%iter (reverse (range 0 isize iprec))])
+                     (define %lastidx1 (- iprec 1))
+                     (define %high (+ %lastidx1 %iter))
+                     (define slice_v1 (extract %high %iter v1))
+                     (define %lastidx2 (- iprec 1))
+                     (define %high2 (+ %lastidx2 %iter))
+                     (define slice_v2 (extract %high2 %iter v2))
+                     (define min-v1-v2 (bvmin slice_v1 slice_v2 sign))
+                     min-v1-v2
+                     )
+           )
+    )
+  dst
+  )
+
+(define (typed:vec-max v1 v2 iprec isize sign)
+  (define dst
+    (apply 
+      concat
+           (for/list ([%iter (reverse (range 0 isize iprec))])
+                     (define %lastidx1 (- iprec 1))
+                     (define %high (+ %lastidx1 %iter))
+                     (define slice_v1 (extract %high %iter v1))
+                     (define %lastidx2 (- iprec 1))
+                     (define %high2 (+ %lastidx2 %iter))
+                     (define slice_v2 (extract %high2 %iter v2))
+                     (define max-v1-v2 (bvmax slice_v1 slice_v2 sign))
+                     max-v1-v2
+                     )
+           )
+    )
+  dst
+  )
+
+(define (typed:vec-lt v1 v2 iprec isize sign)
+  (define dst
+    (apply 
+      concat
+           (for/list ([%iter (reverse (range 0 isize iprec))])
+                     (define %lastidx1 (- iprec 1))
+                     (define %high (+ %lastidx1 %iter))
+                     (define slice_v1 (extract %high %iter v1))
+                     (define %lastidx2 (- iprec 1))
+                     (define %high2 (+ %lastidx2 %iter))
+                     (define slice_v2 (extract %high2 %iter v2))
+                     (define result (bvlt slice_v1 slice_v2 sign))
+                     result
+                     )
+           )
+    )
+  dst
+  )
+
+(define (typed:vec-le v1 v2 iprec isize sign)
+  (define dst
+    (apply 
+      concat
+           (for/list ([%iter (reverse (range 0 isize iprec))])
+                     (define %lastidx1 (- iprec 1))
+                     (define %high (+ %lastidx1 %iter))
+                     (define slice_v1 (extract %high %iter v1))
+                     (define %lastidx2 (- iprec 1))
+                     (define %high2 (+ %lastidx2 %iter))
+                     (define slice_v2 (extract %high2 %iter v2))
+                     (define result (bvle slice_v1 slice_v2 sign))
+                     result
+                     )
+           )
+    )
+  dst
+  )
+
+  (define (typed:vec-bwand v1 v2 iprec isize)
+    (define dst
+      (apply 
+        concat
+            (for/list ([%iter (reverse (range 0 isize iprec))])
+                      (define %lastidx1 (- iprec 1))
+                      (define %high (+ %lastidx1 %iter))
+                      (define slice_v1 (extract %high %iter v1))
+                      (define %lastidx2 (- iprec 1))
+                      (define %high2 (+ %lastidx2 %iter))
+                      (define slice_v2 (extract %high2 %iter v2))
+                      (define %result (bvand slice_v1 slice_v2))
+                      %result
+                      )
+            )
     )
   dst
   )
